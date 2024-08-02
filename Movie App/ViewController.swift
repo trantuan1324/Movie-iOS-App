e99e7b70b08b1304f050d01168d398da4b181ae6
Tuan Tran <quangtuantran1324@gmail.com>
2024-08-02 09:15:43 +0700

    Initial Commit

---
+++ Movie App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie App
+//
+//  Created by Trần Quang Tuấn on 2/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
